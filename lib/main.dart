efe7fa31ef2f388acb3d6a0c058972eff73e49b8
Jordi <jordi.bravo.bad@gmail.com>
2021-07-12 16:39:54 -0400

    Firebase Messagins implementation and Obtaining Device Token

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:push_notifications/src/pages/home_page.dart';
 import 'package:push_notifications/src/pages/message_page.dart';
+import 'package:push_notifications/src/services/push_notifications_services.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await PushNotificationsService.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
--- lib/src/services/push_notifications_services.dart
+++ lib/src/services/push_notifications_services.dart
@@ -1,1 +1,14 @@
 // 65:AE:C5:09:18:56:60:7F:3C:31:A6:97:22:A7:69:DA:05:1A:3D:3A
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class PushNotificationsService {
+  static FirebaseMessaging messagins = FirebaseMessaging.instance;
+  static String? token;
+
+  static Future initializeApp() async {
+    await Firebase.initializeApp();
+    token = await FirebaseMessaging.instance.getToken();
+  }
+}
